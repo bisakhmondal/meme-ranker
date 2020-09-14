466271163da57ad5652742da831d08639895cece
Bisakh Mondal <bisakhmondal00@gmail.com>
2020-09-14 18:52:30 +0530

    New re

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Navigation=({sip})=>{
 
     return(
-        <div className="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow-5 pa0">
+        <div className="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow-5">
            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
